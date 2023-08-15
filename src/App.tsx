b88fe5d23af5ca71908e40a4b4294bbeeb03d9d6
maur32 <maur.martins32@gmail.com>
2023-08-15 19:15:50 -0300

    Fixing post type

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import {Header} from "./components/Header";
-import {Post, PostProps} from "./components/Post";
+import {Post, PostType} from "./components/Post";
 
 import styles from "./App.module.css";
 
 import "./global.css";
 import {Sidebar} from "./components/Sidebar";
 
-interface Post extends PostProps {
-  id: number;
-}
-
-const posts: Post[] = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -59,14 +55,7 @@ function App() {
         <Sidebar />
         <main>
           {posts.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
